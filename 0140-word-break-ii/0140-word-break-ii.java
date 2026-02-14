d75d2b323f4c866792e66db51324250c39f4b142
Er-Lalit <lalitkumar888898@gmail.com>
2026-02-14 17:30:47 +0530

    Time: 2 ms (61.29%), Space: 43.8 MB (52.84%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.java
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -2,35 +2,37 @@ class Solution {
     public List<String> wordBreak(String s, List<String> wordDict) {
         List<String> ans=new ArrayList<>();
         Stack<String> st=new Stack<>();
-         solve(s, st, ans, wordDict,0);  
+        solve(s, st, ans, wordDict);  
         return ans;      
         
     }
-   static void solve(String s,Stack<String> st, List<String> ans,List<String> wordDict,int i)
+    // this was nave approach
+    static void solve(String s,Stack<String> st, List<String> ans,List<String> wordDict)
     {
-        if(i>=s.length())
-        {    
+        if(s.isEmpty())
+        {
             StringBuilder sb=new StringBuilder();
             for(String s1:st)
             {
                 sb.append(s1+" ");
             }
             ans.add(sb.toString().trim());
-            
 
         }
-        for(int j=i;j<s.length();j++)
+        for(int i=0;i<s.length();i++)
         {
-            if(!wordDict.contains(s.substring(i,j+1)))
+            if(!wordDict.contains(s.substring(0,i+1)))
             {
                 continue;
             }
-            if(wordDict.contains(s.substring(i,j+1)))
+            if(wordDict.contains(s.substring(0,i+1)))
             {
-                st.push(s.substring(i,j+1));
-                solve(s, st, ans, wordDict,j+1);
+                st.push(s.substring(0, i+1));
+                solve(s.substring(i+1), st, ans, wordDict);
                 st.pop();
             }
         }
     }
+// some optimized approach
+    //static void sovle(Strin)
 }

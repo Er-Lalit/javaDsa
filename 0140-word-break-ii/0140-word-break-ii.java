f13fb1f8e13d105f0226a39e78b478cf8d6c1752
Er-Lalit <lalitkumar888898@gmail.com>
2026-02-14 17:46:55 +0530

    Time: 1 ms (96.84%), Space: 43.4 MB (60.91%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.java
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -2,37 +2,63 @@ class Solution {
     public List<String> wordBreak(String s, List<String> wordDict) {
         List<String> ans=new ArrayList<>();
         Stack<String> st=new Stack<>();
-        solve(s, st, ans, wordDict);  
+       // solve(s, st, ans, wordDict);  
+       solve(s,"",ans,0,wordDict);
         return ans;      
         
     }
     // this was nave approach
-    static void solve(String s,Stack<String> st, List<String> ans,List<String> wordDict)
-    {
-        if(s.isEmpty())
-        {
-            StringBuilder sb=new StringBuilder();
-            for(String s1:st)
-            {
-                sb.append(s1+" ");
-            }
-            ans.add(sb.toString().trim());
+    // static void solve(String s,Stack<String> st, List<String> ans,List<String> wordDict)
+    // {
+    //     if(s.isEmpty())
+    //     {
+    //         StringBuilder sb=new StringBuilder();
+    //         for(String s1:st)
+    //         {
+    //             sb.append(s1+" ");
+    //         }
+    //         ans.add(sb.toString().trim());
 
-        }
-        for(int i=0;i<s.length();i++)
+    //     }
+    //     for(int i=0;i<s.length();i++)
+    //     {
+    //         if(!wordDict.contains(s.substring(0,i+1)))
+    //         {
+    //             continue;
+    //         }
+    //         if(wordDict.contains(s.substring(0,i+1)))
+    //         {
+    //             st.push(s.substring(0, i+1));
+    //             solve(s.substring(i+1), st, ans, wordDict);
+    //             st.pop();
+    //         }
+    //     }
+    // }
+// some optimized approach
+    static void solve(String s,String currSentence,List<String> ans,int i,List<String> wordDict)
+    {
+        if(i>=s.length())
         {
-            if(!wordDict.contains(s.substring(0,i+1)))
+            ans.add(currSentence);
+            return;
+            
+        }
+        for(int j=i;j<s.length();j++)
+        {
+            String tempword=s.substring(i,j+1);
+            if(wordDict.contains(tempword))
             {
-                continue;
-            }
-            if(wordDict.contains(s.substring(0,i+1)))
-            {
-                st.push(s.substring(0, i+1));
-                solve(s.substring(i+1), st, ans, wordDict);
-                st.pop();
+                String tempSentence=currSentence;
+                if(!currSentence.isEmpty())
+                {
+                    currSentence+=" ";
+                }
+                currSentence+=tempword;
+                solve(s,currSentence,ans,j+1,wordDict);
+                currSentence=tempSentence;
+
+
             }
         }
     }
-// some optimized approach
-    //static void sovle(Strin)
 }

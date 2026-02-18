91286820910e69a7ae46f203486578960248a580
Er-Lalit <lalitkumar888898@gmail.com>
2026-02-18 22:19:44 +0530

    Time: 0 ms (100%), Space: 42.9 MB (36.13%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,9 +1,17 @@
 class Solution {
    static int arr[];
     public int rob(int[] nums) {
-        arr=new int[nums.length];
-        Arrays.fill(arr,-1);
-        return solve(nums,0);
+        // arr=new int[nums.length];
+        // Arrays.fill(arr,-1);
+        // return solve(nums,0);
+        if(nums.length==1)
+        {
+            return nums[0];
+        }
+        int arr[]=new int[nums.length+1];
+        arr[0]=0;
+        arr[1]=nums[0];
+        return solve(nums,arr);
         
     }
     static int solve(int [] nums,int idx)
@@ -23,5 +31,16 @@ class Solution {
         s=solve(nums,idx+1);
         return arr[idx]=Math.max(f,s);
         
+    }
+    // bottom up aaproach
+    static int solve(int nums[],int arr[])
+    {
+        for(int i=2;i<=nums.length;i++)
+        {
+            int steal=nums[i-1]+arr[i-2];
+            int skip=arr[i-1];
+            arr[i]=Math.max(steal,skip);
+        }
+        return arr[nums.length];
     }
 }

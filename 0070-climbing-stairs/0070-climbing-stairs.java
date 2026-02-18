6e9fc6b93f884274cc7d861e4dbdb79edf2c2533
Er-Lalit <lalitkumar888898@gmail.com>
2026-02-18 17:56:59 +0530

    Time: 0 ms (100%), Space: 42.1 MB (54.42%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -4,10 +4,10 @@ class Solution {
         {
             return n;
         }
-        int arr[]=new int[n+1];
-        arr[1]=1;
-        arr[2]=2;
-       return  solve(n,arr);
+        // int arr[]=new int[n+1];
+        // arr[1]=1;
+        // arr[2]=2;
+       return  solve(n,1,2,0);
         
     }
     // with dp memoization
@@ -32,13 +32,24 @@ class Solution {
     //    return arr[n]=count;
     // }
     //bottom up approach
-    static int solve(int n,int arr[])
+    // static int solve(int n,int arr[])
+    // {
+    //     for(int i=3;i<arr.length;i++)
+    //     {
+    //         arr[i]=arr[i-1]+arr[i-2];
+    //     }
+    //     return arr[n];
+    // }
+    // this is  is for constant space complexity
+    static int solve(int n,int a, int b,int c)
     {
-        for(int i=3;i<arr.length;i++)
+        for(int i=3;i<=n;i++)
         {
-            arr[i]=arr[i-1]+arr[i-2];
+            c=a+b;
+            a=b;
+            b=c;
         }
-        return arr[n];
+        return c;
     }
 
     

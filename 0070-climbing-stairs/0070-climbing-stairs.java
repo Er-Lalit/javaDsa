1a7e8471496215e62620979f4f89246567c42869
Er-Lalit <lalitkumar888898@gmail.com>
2026-02-18 17:51:06 +0530

    Time: 0 ms (100%), Space: 42 MB (73.28%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,29 +1,46 @@
 class Solution {
     public int climbStairs(int n) {
+        if(n<=2)
+        {
+            return n;
+        }
         int arr[]=new int[n+1];
+        arr[1]=1;
+        arr[2]=2;
        return  solve(n,arr);
         
     }
+    // with dp memoization
+    // static int solve(int n,int arr[])
+    // {
+    //     int count=0;
+        
+    //     if(n==0)
+    //     {
+    //         return 1;
+    //     }
+    //     else if(n<0)
+    //     {
+    //         return 0;
+    //     }
+    //     if(arr[n]!=0)
+    //     {
+    //         return arr[n];
+    //     }
+    //    count+= solve(n-1,arr);
+    //    count+= solve(n-2,arr);
+    //    return arr[n]=count;
+    // }
+    //bottom up approach
     static int solve(int n,int arr[])
     {
-        int count=0;
-        
-        if(n==0)
+        for(int i=3;i<arr.length;i++)
         {
-            return 1;
+            arr[i]=arr[i-1]+arr[i-2];
         }
-        else if(n<0)
-        {
-            return 0;
-        }
-        if(arr[n]!=0)
-        {
-            return arr[n];
-        }
-       count+= solve(n-1,arr);
-       count+= solve(n-2,arr);
-       return arr[n]=count;
+        return arr[n];
     }
+
     
     
 }

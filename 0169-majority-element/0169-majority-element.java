34e479dbfd37f7b76416a09939a43e942b38e77f
Er-Lalit <lalitkumar888898@gmail.com>
2026-02-23 11:50:08 +0530

    Time: 10 ms (34.54%), Space: 55.7 MB (47.9%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,25 +1,47 @@
 class Solution {
-    public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map=new HashMap();
-        for(int i=0;i<nums.length;i++)
+     public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        int frequency=1;
+        for(int i=1;i<nums.length;i++)
         {
-            if(map.containsKey(nums[i]))
+            if(nums[i]==nums[i-1])
             {
-                map.put(nums[i],map.get(nums[i])+1);
+                frequency+=1;
+                if(frequency>nums.length/2)
+                {
+                    return nums[i];
+                }
             }
             else
             {
-                map.put(nums[i],1);
+                frequency=1;
             }
         }
-        for(int key:map.keySet())
-        {
-            if(map.get(key)>nums.length/2)
-            {
-                return key;
-            }
-        }
-        return 0;
-        
+        return nums[0];
     }
+
+    // THIS IS BRUTFORCE APPROACH
+    // public int majorityElement(int[] nums) {
+    //     HashMap<Integer,Integer> map=new HashMap();
+    //     for(int i=0;i<nums.length;i++)
+    //     {
+    //         if(map.containsKey(nums[i]))
+    //         {
+    //             map.put(nums[i],map.get(nums[i])+1);
+    //         }
+    //         else
+    //         {
+    //             map.put(nums[i],1);
+    //         }
+    //     }
+    //     for(int key:map.keySet())
+    //     {
+    //         if(map.get(key)>nums.length/2)
+    //         {
+    //             return key;
+    //         }
+    //     }
+    //     return 0;
+        
+    // }
 }

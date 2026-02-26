a6d2d09e9b385c61c87149e1092e58597125f279
Er-Lalit <lalitkumar888898@gmail.com>
2026-02-26 23:29:00 +0530

    Time: 0 ms (100%), Space: 44.3 MB (68.16%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -9,25 +9,48 @@
  * }
  */
 class Solution {
-    static ListNode head1;
     public ListNode reverseList(ListNode head) {
-        head1=head;
-        recu(head);
-        return head1;
+        if(head==null)
+        {
+            return head;
+        }
+        ListNode prev=null;
+        ListNode present=head;
+        ListNode nxt=present.next;
+        while (present!=null)
+        {
+            present.next=prev;
+            prev=present;
+            present=nxt;
+            if(nxt!=null)
+            {
+                nxt=nxt.next;
+
+            }
+           
+        } 
+        return head=prev;  
+    }
+    // this was the recursive approach
+    // static ListNode head1;
+    // public ListNode reverseList(ListNode head) {
+    //     head1=head;
+    //     recu(head);
+    //     return head1;
 
         
         
-    }
-    static ListNode recu(ListNode node)
-    {
-        if(node==null || node.next==null)
-        {
-            head1=node;
-            return node;
-        }
-        ListNode newNode=recu(node.next);
-        newNode.next=node;
-        node.next=null;
-        return node;
-    }
+    // }
+    // static ListNode recu(ListNode node)
+    // {
+    //     if(node==null || node.next==null)
+    //     {
+    //         head1=node;
+    //         return node;
+    //     }
+    //     ListNode newNode=recu(node.next);
+    //     newNode.next=node;
+    //     node.next=null;
+    //     return node;
+    // }
 }

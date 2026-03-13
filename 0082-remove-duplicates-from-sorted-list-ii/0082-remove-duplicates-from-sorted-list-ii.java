fecbd64b70364c2fdee3d56eece6fa636af949a2
Er-Lalit <lalitkumar888898@gmail.com>
2026-03-13 22:05:03 +0530

    Time: 0 ms (100%), Space: 44.9 MB (84%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
@@ -8,63 +8,94 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+ // this was the brut force approach 
+//class Solution {
+//     public ListNode deleteDuplicates(ListNode head) {
+//         if(head==null || head.next==null)
+//         {
+//             return head;
+//         }
+//        HashMap<Integer,Integer> map=new HashMap();
+//        ListNode curr=head;
+//        while(curr!=null)
+//        {
+//           if(!map.containsKey(curr.val))
+//           {
+//             map.put(curr.val,1);
+//           }
+//           else
+//           {
+//             int valInKey=map.get(curr.val);
+//             map.put(curr.val,valInKey+1);
+
+//           }
+//           curr=curr.next;
+//         }
+//         // if here one val in each node 
+
+//         if(map.size()==1)
+//         {
+//             return null;
+//         }
+//         curr=head;
+//         ListNode prev=null;
+//         while(curr!=null)
+//         {
+//             if(prev==null && map.get(curr.val)<2)
+//             {
+//                 head=curr;
+//                 prev=curr;
+//                 curr=curr.next;
+//                 continue;
+//             }
+//             if(map.get(curr.val)<2)
+//             {
+//                 prev.next=curr;
+//                 prev=curr;
+//                 curr=curr.next;
+//             }
+//             else
+//             {
+//                 curr=curr.next;
+//             }
+
+//         }
+//         if(prev==null)
+//         {
+//             return null;
+//         }
+//         prev.next=null;
+//         return head;
+        
+//     }
+// }
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
         if(head==null || head.next==null)
         {
             return head;
         }
-       HashMap<Integer,Integer> map=new HashMap();
-       ListNode curr=head;
-       while(curr!=null)
-       {
-          if(!map.containsKey(curr.val))
-          {
-            map.put(curr.val,1);
-          }
-          else
-          {
-            int valInKey=map.get(curr.val);
-            map.put(curr.val,valInKey+1);
-
-          }
-          curr=curr.next;
-        }
-        // if here one val in each node 
-
-        if(map.size()==1)
-        {
-            return null;
-        }
-        curr=head;
-        ListNode prev=null;
+        ListNode dummy=new ListNode(0);
+        dummy.next=head;
+        ListNode prev=dummy;
+        ListNode curr=head;
         while(curr!=null)
         {
-            if(prev==null && map.get(curr.val)<2)
+            if(curr.next!=null && curr.val==curr.next.val)
             {
-                head=curr;
-                prev=curr;
-                curr=curr.next;
-                continue;
-            }
-            if(map.get(curr.val)<2)
-            {
-                prev.next=curr;
-                prev=curr;
-                curr=curr.next;
+                while(curr.next!=null && curr.val ==curr.next.val)
+                {
+                    curr=curr.next;
+                }
+                prev.next=curr.next;
             }
             else
             {
-                curr=curr.next;
+                prev=prev.next;
             }
+            curr=curr.next;
+        }
+        return dummy.next;
 
-        }
-        if(prev==null)
-        {
-            return null;
-        }
-        prev.next=null;
-        return head;
-        
     }
-}
+}    

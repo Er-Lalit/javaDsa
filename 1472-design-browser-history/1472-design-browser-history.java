0f3d8697ec830a539803bf72315a80a531592285
Er-Lalit <lalitkumar888898@gmail.com>
2026-03-14 11:50:02 +0530

    Time: 48 ms (90.76%), Space: 52.5 MB (64.72%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.java
+++ 1472-design-browser-history/1472-design-browser-history.java
@@ -1,50 +1,110 @@
+// this implement using the stack 
+// class BrowserHistory {
+//     Stack<String> past;
+//     Stack<String> future;
+//     String curr;
+
+//     public BrowserHistory(String homepage) {
+//         past=new Stack();
+//         future=new Stack();
+//         past.push(homepage);
+//         curr=homepage;
+        
+//     }
+    
+//     public void visit(String url) {
+//         if(!future.isEmpty())
+//         {
+//             future.clear();
+//         }
+//         past.push(url);
+//         curr=url;
+        
+//     }
+    
+//     public String back(int steps) {
+//         while(steps>0 && past.size()>1)
+//         {
+            
+//             future.push(past.pop());
+//             steps--;
+//         }
+//         return curr=past.peek();
+        
+//     }
+    
+//     public String forward(int steps) {
+//         if(future.isEmpty())
+//         {
+//             return curr;
+//         }
+//         while(steps>0 && !future.isEmpty())
+//         {
+//             curr=future.pop();
+//             past.push(curr);
+//             steps--;
+           
+//         }
+//         return curr;
+//     }
+// }
+
+// try to implement using the doulbly linkedlist 
+
+
 class BrowserHistory {
-    Stack<String> past;
-    Stack<String> future;
-    String curr;
+    Node head;
+    Node curr;
+    private class Node
+    {
+        Node prev;
+        Node next;
+        String url;
+        Node(String url)
+        {
+            this.url=url;
+        }
+
+    }
+    
 
     public BrowserHistory(String homepage) {
-        past=new Stack();
-        future=new Stack();
-        past.push(homepage);
-        curr=homepage;
-        
+        Node node=new Node(homepage);
+        head=node;
+        curr=node;
+       
     }
     
     public void visit(String url) {
-        if(!future.isEmpty())
-        {
-            future.clear();
-        }
-        past.push(url);
-        curr=url;
+        Node node=new Node(url);
+        curr.next=node;
+        node.prev=curr;
+        curr=curr.next;
+
+       
         
     }
     
     public String back(int steps) {
-        while(steps>0 && past.size()>1)
+        while(curr.prev!=null && steps>0)
         {
-            
-            future.push(past.pop());
+            curr=curr.prev;
             steps--;
         }
-        return curr=past.peek();
+        return curr.url;
+        
+        
         
     }
     
     public String forward(int steps) {
-        if(future.isEmpty())
+        while(curr.next!=null && steps>0)
         {
-            return curr;
-        }
-        while(steps>0 && !future.isEmpty())
-        {
-            curr=future.pop();
-            past.push(curr);
+            curr=curr.next;
             steps--;
-           
         }
-        return curr;
+        return curr.url;
+       
     }
 }
 

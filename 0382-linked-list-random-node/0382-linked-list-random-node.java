b1ae8d426a8630488f8b30a40c8efd4c759a993e
Er-Lalit <lalitkumar888898@gmail.com>
2026-03-15 18:07:10 +0530

    Time: 13 ms (22.54%), Space: 48.5 MB (47.63%) - LeetHub

--- 0382-linked-list-random-node/0382-linked-list-random-node.java
+++ 0382-linked-list-random-node/0382-linked-list-random-node.java
@@ -8,28 +8,50 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-    List<Integer> list;
+// class Solution {
+//     List<Integer> list;
     
-    public Solution(ListNode head) {
-        list=new ArrayList<>();
-        ListNode temp=head;
-        while(temp!=null)
-        {
-            list.add(temp.val);
-            temp=temp.next;
-        }
+//     public Solution(ListNode head) {
+//         list=new ArrayList<>();
+//         ListNode temp=head;
+//         while(temp!=null)
+//         {
+//             list.add(temp.val);
+//             temp=temp.next;
+//         }
         
         
         
-    }
+//     }
     
-    public int getRandom() {
-        int len=list.size();
-        int idx=(int)(Math.random()*len)%len;
-       return list.get(idx);
+//     public int getRandom() {
+//         int len=list.size();
+//         int idx=(int)(Math.random()*len)%len;
+//        return list.get(idx);
        
         
+//     }
+// Reservoir Sampling 
+class Solution {
+    ListNode Head;
+    public Solution(ListNode head) {
+        Head=head;
+    }
+    public int getRandom() {
+        ListNode temp=Head;
+        int ans=0;
+        int count=1;
+        while(temp!=null)
+        {
+            if((Math.random()%count)<1.0/count)
+            {
+                ans=temp.val;
+
+            }
+            count++;
+            temp=temp.next;
+        }
+        return ans;
     }
 }
 

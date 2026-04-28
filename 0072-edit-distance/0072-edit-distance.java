3446f30f6eec15200ac17fadee9dc0c91142c94a
Er-Lalit <lalitkumar888898@gmail.com>
2026-04-28 12:33:33 +0530

    Time: 4 ms (78.15%), Space: 47 MB (71.03%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -6,27 +6,23 @@ class Solution {
         {
             Arrays.fill(dp[i],-1);
         }
-        return solve(word1,word2,0,0);
+        return solve(word1,word2,word1.length(),word2.length());
         
     }
     static int solve(String s1,String s2,int i,int j)
     {
-        if(j==s2.length())
+        if(i==0 || j==0)
         {
-            return s1.length()-i;
-        }
-        if(i==s1.length())
-        {
-            return s2.length()-j;
+            return i+j;
         }
         if(dp[i][j]!=-1)
         {
             return dp[i][j];
         }
-        if(s1.charAt(i)==s2.charAt(j))
+        if(s1.charAt(i-1)==s2.charAt(j-1))
         {
-            return solve(s1,s2,i+1,j+1);
+            return solve(s1,s2,i-1,j-1);
         }
-      return  dp[i][j]= Math.min(1+solve(s1,s2,i,j+1),Math.min(1+solve(s1,s2,i+1,j),1+solve(s1,s2,i+1,j+1)));
+      return  dp[i][j]= Math.min(1+solve(s1,s2,i,j-1),Math.min(1+solve(s1,s2,i-1,j),1+solve(s1,s2,i-1,j-1)));
     }
 }

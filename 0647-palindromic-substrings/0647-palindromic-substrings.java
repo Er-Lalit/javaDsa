09534dd46064ac54a9df132b0dd4a71befd1fbe3
Er-Lalit <lalitkumar888898@gmail.com>
2026-04-28 22:59:39 +0530

    Time: 19 ms (26.75%), Space: 46.3 MB (28.45%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,39 +1,69 @@
 class Solution {
-   static boolean dp[][];
-    public int countSubstrings(String s) {
-        dp=new boolean[s.length()+1][s.length()+1];
+//    static boolean dp[][];
+//     public int countSubstrings(String s) {
+//         dp=new boolean[s.length()+1][s.length()+1];
 
-        int count=0;
-        for(int i=0;i<s.length();i++)
+//         int count=0;
+//         for(int i=0;i<s.length();i++)
+//         {
+//             for(int j=i;j<s.length();j++)
+//             {
+//                 if(check(i,j,s))
+//                 {
+//                     count+=1;
+//                 }
+//             }
+//         }
+//         return count;
+//     }  
+//     static boolean check(int i ,int j,String s)
+//     {
+//         if(i>=j)
+//         {
+//             return true;
+//         }
+//         if(dp[i][j]!=false)
+//         {
+//             return dp[i][j];
+//         }
+//         if(s.charAt(i)==s.charAt(j))
+//         {
+//             return dp[i][j]=check(i+1,j-1,s);
+//         }
+//         else
+//         {
+//             return dp[i][j]=false;
+//         }
+        
+//     }  
+public int countSubstrings(String s) {
+    boolean dp[][]=new boolean[s.length()][s.length()];
+    int count=0;
+    int j=0;
+    for(int l=1;l<=s.length();l++)
+    {
+        for(int i=0;l+i-1<s.length();i++)
         {
-            for(int j=i;j<s.length();j++)
+            j=l+i-1;
+            if(i==j)
             {
-                if(check(i,j,s))
-                {
-                    count+=1;
-                }
+                dp[i][j]=true;
+            }
+            else if(i+1==j)
+            {
+                dp[i][j]=(s.charAt(i)==s.charAt(j));
+            }
+            else
+            {
+                dp[i][j]=(dp[i+1][j-1] && s.charAt(i)==s.charAt(j));
+            }
+            if(dp[i][j]==true)
+            {
+                count++;
             }
         }
-        return count;
-    }  
-    static boolean check(int i ,int j,String s)
-    {
-        if(i>=j)
-        {
-            return true;
-        }
-        if(dp[i][j]!=false)
-        {
-            return dp[i][j];
-        }
-        if(s.charAt(i)==s.charAt(j))
-        {
-            return dp[i][j]=check(i+1,j-1,s);
-        }
-        else
-        {
-            return dp[i][j]=false;
-        }
-        
-    }  
+    }
+    return count;
+}
+
 }

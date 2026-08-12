9813f7e3dd34cac657e66385c9de7b92a95747cc
Er-Lalit <lalitkumar888898@gmail.com>
2026-08-12 17:23:52 +0530

    solve the undirectedgraphcycle question

---
+++ GeeksForGeeks/UndirectedGraphCycle/UndirectedGraphCycle.java
@@ -0,0 +1,59 @@
+package GeeksForGeeks.UndirectedGraphCycle;
+
+import java.util.ArrayList;
+
+public class UndirectedGraphCycle {
+
+    class Solution {
+    public boolean isCycle(int V, int[][] edges) {
+       ArrayList<ArrayList<Integer>> adj=new ArrayList();
+       for(int i=0;i<V;i++)
+       {
+           adj.add(new ArrayList());
+       }
+       
+       for(int[] edge:edges)
+       {
+           int u=edge[0];
+           int v=edge[1];
+           adj.get(u).add(v);
+           adj.get(v).add(u);
+       }
+       
+       boolean visited[]=new boolean[V];
+       
+       for(int i=0;i<V;i++)
+       {
+           if(!visited[i] && isCycleDfs(adj,i,visited,-1))
+           {
+               return true;
+           }
+       }
+       return false;
+        
+    }
+    static boolean isCycleDfs(ArrayList<ArrayList<Integer>> adj,int u,boolean visited[],int parent)
+    {
+        visited[u]=true;
+        for(int v:adj.get(u))
+        {
+            if(v==parent)
+            {
+                continue;
+            }
+            
+            if(visited[v])
+            {
+                return true;
+            }
+            if(isCycleDfs(adj,v,visited,u))
+            {
+                return true;
+            }
+        }
+        return false;
+        
+    }
+}
+    
+}

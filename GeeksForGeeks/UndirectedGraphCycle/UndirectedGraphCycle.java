4e555e9fa2670d7d64e24877a09369ef5c8368fe
Er-Lalit <lalitkumar888898@gmail.com>
2026-08-12 22:10:14 +0530

    add the bfs way to detect cyle in the undirected graph

--- GeeksForGeeks/UndirectedGraphCycle/UndirectedGraphCycle.java
+++ GeeksForGeeks/UndirectedGraphCycle/UndirectedGraphCycle.java
@@ -4,7 +4,63 @@ import java.util.ArrayList;
 
 public class UndirectedGraphCycle {
 
-    class Solution {
+    // cycle detection using the dfs
+
+//     class Solution {
+//     public boolean isCycle(int V, int[][] edges) {
+//        ArrayList<ArrayList<Integer>> adj=new ArrayList();
+//        for(int i=0;i<V;i++)
+//        {
+//            adj.add(new ArrayList());
+//        }
+       
+//        for(int[] edge:edges)
+//        {
+//            int u=edge[0];
+//            int v=edge[1];
+//            adj.get(u).add(v);
+//            adj.get(v).add(u);
+//        }
+       
+//        boolean visited[]=new boolean[V];
+       
+//        for(int i=0;i<V;i++)
+//        {
+//            if(!visited[i] && isCycleDfs(adj,i,visited,-1))
+//            {
+//                return true;
+//            }
+//        }
+//        return false;
+        
+//     }
+//     static boolean isCycleDfs(ArrayList<ArrayList<Integer>> adj,int u,boolean visited[],int parent)
+//     {
+//         visited[u]=true;
+//         for(int v:adj.get(u))
+//         {
+//             if(v==parent)
+//             {
+//                 continue;
+//             }
+            
+//             if(visited[v])
+//             {
+//                 return true;
+//             }
+//             if(isCycleDfs(adj,v,visited,u))
+//             {
+//                 return true;
+//             }
+//         }
+//         return false;
+        
+//     }
+// }
+
+// **** cycle detection using the bfs *** 
+
+class Solution {
     public boolean isCycle(int V, int[][] edges) {
        ArrayList<ArrayList<Integer>> adj=new ArrayList();
        for(int i=0;i<V;i++)
@@ -24,7 +80,7 @@ public class UndirectedGraphCycle {
        
        for(int i=0;i<V;i++)
        {
-           if(!visited[i] && isCycleDfs(adj,i,visited,-1))
+           if(!visited[i] && isCycleBfs(adj,i,visited))
            {
                return true;
            }
@@ -32,27 +88,32 @@ public class UndirectedGraphCycle {
        return false;
         
     }
-    static boolean isCycleDfs(ArrayList<ArrayList<Integer>> adj,int u,boolean visited[],int parent)
+    static boolean isCycleBfs(ArrayList<ArrayList<Integer>> adj,int u,boolean visited[])
     {
         visited[u]=true;
-        for(int v:adj.get(u))
+        Queue<ArrayList<Integer>> q=new ArrayDeque();
+        q.offer(new ArrayList<>(Arrays.asList(u,-1)));
+        while(!q.isEmpty())
         {
-            if(v==parent)
+            ArrayList<Integer> list=q.poll();
+            int source=list.get(0);
+            int parent=list.get(1);
+            for(int v:adj.get(source))
             {
-                continue;
+                if(visited[v]==false)
+                {
+                    visited[v]=true;
+                    q.offer(new ArrayList<>(Arrays.asList(v,source)));
+                    
+                }
+                else if(v!=parent)
+                {
+                    return true;
+                }
             }
-            
-            if(visited[v])
-            {
-                return true;
-            }
-            if(isCycleDfs(adj,v,visited,u))
-            {
-                return true;
-            }
+           
         }
         return false;
-        
     }
 }
     
